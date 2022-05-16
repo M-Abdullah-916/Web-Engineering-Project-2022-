ae4eb6c34f4712a9775c9d89a4f0b6352b659f3b
M-Abdullah-916 <f190916@nu.edu.pk>
2022-05-16 19:59:09 +0500

    Books Started to Add in DB

--- routes/book.route.js
+++ routes/book.route.js
@@ -5,5 +5,6 @@ const book_controller = require("../controllers/books.controller");
 
 router.get("/add", book_controller.add);
 router.post("/add", book_controller.create);
+router.get("/all", book_controller.all);
 
 module.exports = router;
